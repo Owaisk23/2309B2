19df1a43e4b3df560979103af145132d5f422f5e
Owais Ahmed Khan <owais1566@gmail.com>
2024-06-29 17:40:44 +0500

    added php class code

---
+++ PHP/Basics/form.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Super Global Variable In PHP</h1>
+    <form action="./submit.php" method="get">
+        <input type="text" name="username" placeholder="Enter Username" required> <br><br>
+        <input type="email" name="useremail" placeholder="Enter Useremail" required><br> <br>
+        <input type="submit" name="userinfo" value="Submit">
+    </form>
+</body>
+</html>
---
+++ PHP/Basics/submit.php
@@ -0,0 +1,15 @@
+ <?php
+// echo "<pre>";
+// print_r($_GET);
+// echo "</pre>";
+
+// echo "<pre>";
+// print_r($_POST);
+// echo "</pre>";
+
+// echo "<pre>";
+// print_r($_REQUEST);
+// echo "</pre>";
+
+echo "<h1>Welcome to our company Mr/Ms. ". ucwords($_GET['username']) . " and User Email is ". $_GET['useremail'] ." </h1>"
+?>
