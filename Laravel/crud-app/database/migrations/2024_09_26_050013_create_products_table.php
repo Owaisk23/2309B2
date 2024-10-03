25bf8001e092cc09f6d0bd2e4976ae72b8d755ea
Owais Ahmed Khan <owais1566@gmail.com>
2024-10-03 11:02:59 +0500

    laravel crud

--- Laravel/crud-app/database/migrations/2024_09_26_050013_create_products_table.php
+++ Laravel/crud-app/database/migrations/2024_09_26_050013_create_products_table.php
@@ -11,7 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 70);
+            $table->text('description');
+            $table->string('image', 100);
+            $table->timestamps();
+        });
     }
 
     /**
@@ -19,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('products');
     }
 };
