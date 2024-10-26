42273e9bfbd0b8c839e6b6a58d43971e286e49ec
Owais Ahmed Khan <owais1566@gmail.com>
2024-10-26 12:57:13 +0500

    added TS Code

--- TS/app.js
+++ TS/app.js
@@ -1,21 +1,74 @@
 console.log("Hello in Typescript!!");
 // alert("Good Morning!")
-var userName = "Owais Ahmed Khan";
-console.log(userName);
-console.log(typeof userName);
-console.log(userName.toUpperCase());
-console.log(userName.toLowerCase());
-var userAge = 23;
-var userEmail = "owais23@gmail.com";
-console.log(userAge);
-console.log(userEmail);
-var userDetails = "User Name is ".concat(userName, " Age is ").concat(userAge, " and User Email is ").concat(userEmail);
-console.log(userDetails);
+// let userName: string = "Owais Ahmed Khan";
+// console.log(userName);
+// console.log(typeof userName);
+// console.log(userName.toUpperCase())
+// console.log(userName.toLowerCase())
+// let userAge: number = 23;
+// let userEmail: string = "owais23@gmail.com";
+// console.log(userAge)
+// console.log(userEmail)
+// let userDetails: string = `User Name is ${userName} Age is ${userAge} and User Email is ${userEmail}`;
+// console.log(userDetails);
 // true:1 false:0 null:0
-var userPost = null;
-if (userPost) {
-    console.log("Null true huta hai");
-}
-else {
-    console.log("Null False huta hai");
-}
+// let userPost = null;
+// if(userPost){
+//     console.log("Null true huta hai")
+// }else{
+//     console.log("Null False huta hai")
+// }
+// let userPost = undefined;
+// if(userPost){
+//     console.log("Null true huta hai")
+// }else{
+//     console.log("Null False huta hai")
+// }
+// let stdMarks: number[] = [77, 90, 82, 67, 99];
+// console.log(stdMarks)
+// let stdNames: string[] = ["Ayan", "Sami", "Ahsan", "Hunain", "Luqman", "Hanzala", "Daniyal"]
+// console.log(stdNames)
+// // stdNames.forEach((e) => {
+// //     console.log(e)
+// // })
+// let stds = stdNames.map((e) => {
+//     // console.log(e.toUpperCase())
+//     return e.toUpperCase();
+// })
+// console.log(stds);
+// Object
+// type any
+// let product: any = {
+//     prodName: "IPhone 16 Pro Max",
+//     prodColor: "Purple",
+//     prodPrice: 400000,
+//     prodStock: true
+// }
+// console.log(product)
+// console.log(product.prodName)
+// console.log(product.prodPrice)
+// let student: object = {
+//     stdName: "Ayan",
+//     stdAge: 23,
+//     stdExamStatus: true
+// }
+// console.log(student)
+// let student: {
+//   stdName: string;
+//   stdAge: number;
+//   stdExamStatus: boolean;
+// } = {
+//   stdName: "Ayan",
+//   stdAge: 23,
+//   stdExamStatus: true,
+// };
+var mixedDT;
+mixedDT = ["Apple", "Grapes", "Banana", 89, 110, 90];
+mixedDT.forEach(function (e) {
+    if (typeof e == "string") {
+        console.log(e.toUpperCase());
+    }
+    else {
+        console.log(e.toFixed(2));
+    }
+});
--- TS/app.ts
+++ TS/app.ts
@@ -1,33 +1,124 @@
-console.log("Hello in Typescript!!")
+console.log("Hello in Typescript!!");
 
 // alert("Good Morning!")
 
-let userName: string = "Owais Ahmed Khan";
+// let userName: string = "Owais Ahmed Khan";
 
-console.log(userName);
-console.log(typeof userName);
-console.log(userName.toUpperCase())
-console.log(userName.toLowerCase())
+// console.log(userName);
+// console.log(typeof userName);
+// console.log(userName.toUpperCase())
+// console.log(userName.toLowerCase())
 
-let userAge: number = 23;
+// let userAge: number = 23;
 
-let userEmail: string = "owais23@gmail.com";
+// let userEmail: string = "owais23@gmail.com";
 
-console.log(userAge)
-console.log(userEmail)
+// console.log(userAge)
+// console.log(userEmail)
 
-let userDetails: string = `User Name is ${userName} Age is ${userAge} and User Email is ${userEmail}`;
+// let userDetails: string = `User Name is ${userName} Age is ${userAge} and User Email is ${userEmail}`;
 
-console.log(userDetails);
-
+// console.log(userDetails);
 
 // true:1 false:0 null:0
 
-let userPost = null;
+// let userPost = null;
 
-if(userPost){
-    console.log("Null true huta hai")
-}else{
-    console.log("Null False huta hai")
-}
+// if(userPost){
+//     console.log("Null true huta hai")
+// }else{
+//     console.log("Null False huta hai")
+// }
+
+// let userPost = undefined;
+
+// if(userPost){
+//     console.log("Null true huta hai")
+// }else{
+//     console.log("Null False huta hai")
+// }
+
+// let stdMarks: number[] = [77, 90, 82, 67, 99];
+
+// console.log(stdMarks)
+
+// let stdNames: string[] = ["Ayan", "Sami", "Ahsan", "Hunain", "Luqman", "Hanzala", "Daniyal"]
+
+// console.log(stdNames)
+
+// // stdNames.forEach((e) => {
+// //     console.log(e)
+// // })
+
+// let stds = stdNames.map((e) => {
+//     // console.log(e.toUpperCase())
+//     return e.toUpperCase();
+// })
+
+// console.log(stds);
+
+// Object
+// type any
+
+// let product: any = {
+//     prodName: "IPhone 16 Pro Max",
+//     prodColor: "Purple",
+//     prodPrice: 400000,
+//     prodStock: true
+// }
+
+// console.log(product)
+// console.log(product.prodName)
+// console.log(product.prodPrice)
+
+// let student: object = {
+//     stdName: "Ayan",
+//     stdAge: 23,
+//     stdExamStatus: true
+// }
+
+// console.log(student)
+
+// let student: {
+//   stdName: string;
+//   stdAge: number;
+//   stdExamStatus: boolean;
+// } = {
+//   stdName: "Ayan",
+//   stdAge: 23,
+//   stdExamStatus: true,
+// };
+
+let mixedDT: (string | number) [];
+
+mixedDT = ["Apple", "Grapes", "Banana", 89, 110, 90];
+
+mixedDT.forEach((e) => {
+  if(typeof e == "string"){
+    console.log(e.toUpperCase())
+  }else{
+    console.log(e.toFixed(2))
+  }
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
