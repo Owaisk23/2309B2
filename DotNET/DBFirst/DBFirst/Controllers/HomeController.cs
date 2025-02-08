7797f36c06f807f4c60de48393cc3e5b59e56949
Owais Ahmed <owaisahmed@aptech.com>
2025-02-08 10:21:18 +0500

    added search in basic crud

--- DotNET/DBFirst/DBFirst/Controllers/HomeController.cs
+++ DotNET/DBFirst/DBFirst/Controllers/HomeController.cs
@@ -18,6 +18,65 @@ namespace DBFirst.Controllers
             return View(db.Products.ToList());
         }
 
+        [HttpPost]
+        public IActionResult Index(string searchQuery)
+        {
+            // Retrieve products from the database
+            var products = string.IsNullOrEmpty(searchQuery)
+            ? db.Products.ToList() // If no search, return all products
+                : db.Products
+                    .Where(p => p.Name.Contains(searchQuery) || p.Description.Contains(searchQuery))
+                    .ToList(); // Search logic
+
+            ViewData["SearchQuery"] = searchQuery; // Pass search query back to view
+            return View(products);
+        }
+
+        public IActionResult AddProduct()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult AddProduct(Product product)
+        {
+            db.Products.Add(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult EditProduct(int id)
+        {
+            var product = db.Products.FirstOrDefault(p => p.Id == id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult EditProduct(Product product)
+        {
+            db.Products.Update(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public IActionResult DeleteProduct(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.Id == id);
+            return View(product);
+        }
+        [HttpPost]
+        public IActionResult DeleteProduct(Product product)
+        {
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult DetailProduct(int id)
+        {
+            var product = db.Products.FirstOrDefault(x => x.Id == id);
+            return View(product);
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
