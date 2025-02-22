67b02f8b5d559a3bb2e36d23edfec82c4769bbfc
Owais Ahmed <owaisahmed@aptech.com>
2025-02-22 11:03:40 +0500

    rolebaseauthentication

---
+++ DotNET/AuthenticationwithSession/AuthenticationwithSession/Controllers/ClientController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AuthenticationwithSession.Controllers
+{
+    public class ClientController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return View();
+            if (HttpContext.Session.GetString("role") == "client")
+            {
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+        }
+    }
+}
---
+++ DotNET/AuthenticationwithSession/AuthenticationwithSession/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using AuthenticationwithSession.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace AuthenticationwithSession.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            //return View();
+            if (HttpContext.Session.GetString("role") == "user")
+            {
+
+                return View();
+            }
+            else
+            {
+                return RedirectToAction("Login", "Admin");
+            }
+        }
+
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
