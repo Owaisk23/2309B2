883365bdad5b060fdef7cda025cf99a32031f87c
owaisk23 <owais1566@gmail.com>
2025-05-08 11:06:47 +0500

    added basics

--- Mob App Development/Dart/basics/lib/basics.dart
+++ Mob App Development/Dart/basics/lib/basics.dart
@@ -1,3 +1,11 @@
+import 'dart:io';
+
 void main(){
   print("Welcome to Dart Programming!");
+  print("Welcome to Flutter Programming!");
+
+  stdout.write("Enter your first name: \n");
+  stdout.write("Enter your last name: ");
+
+
 }
