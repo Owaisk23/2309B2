31dd8e6955a0cb74ea274e2d38dcc3e52e6e855f
owaisk23 <owais1566@gmail.com>
2025-05-22 10:05:13 +0500

    added api fetch

--- Mob App Development/Dart/api_fetch/lib/api_fetch.dart
+++ Mob App Development/Dart/api_fetch/lib/api_fetch.dart
@@ -1,11 +1,36 @@
-import 'package:http/http.dart' as http;
+import 'dart:convert' as CONVERT;
+import 'package:http/http.dart' as HTTP;
 
 void main() async{
   print("Dart API Fetch");
 
   final API = Uri.parse("https://67d13a6e825945773eb37cfc.mockapi.io/dev/students");
-  final response = await http.get(API);
-  print(response.body);
+
+  HTTP.get(API).then((response) {
+    if (response.statusCode == 200) {
+      final data = CONVERT.jsonDecode(response.body);
+      // print(data);
+      List<dynamic> studentsInfo = data;
+      for (var student in studentsInfo) {
+        print("Student Name: ${student['name']}");
+        print("Student Avatar: ${student['avatar']}");
+        print("Student Age: ${student['age']}");
+        print("Student Age: ${student['country']}");
+        print("================================================================");
+      }
+    
+    } else {
+      print("Error: ${response.statusCode}");
+    }
+ 
+  });
+
+
+
+
+  
+  // final response = await http.get(API);
+  // print(response.body);
 
 
 
