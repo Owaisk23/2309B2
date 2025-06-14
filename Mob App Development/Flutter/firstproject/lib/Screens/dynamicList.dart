802dd7389b3420f91828980895869c6b238f6c39
owaisk23 <owais1566@gmail.com>
2025-06-14 10:05:43 +0500

    added cars array

--- Mob App Development/Flutter/firstproject/lib/Screens/dynamicList.dart
+++ Mob App Development/Flutter/firstproject/lib/Screens/dynamicList.dart
@@ -39,28 +39,81 @@ class _DynamicListState extends State<DynamicList> {
       'name': 'General Asim Munir',
     },
   ];
-
-
+  
+var Cars = [
+    {
+      "name": "BMW",
+      "model": "X5",
+      "price": 50000,
+      "color": "Black",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Audi",
+      "model": "A6",
+      "price": 60000,
+      "color": "White",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Mercedes",
+      "model": "C-Class",
+      "price": 70000,
+      "color": "Silver",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Toyota",
+      "model": "Camry",
+      "price": 30000,
+      "color": "Blue",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    },
+    {
+      "name": "Honda",
+      "model": "Accord",
+      "price": 25000,
+      "color": "Red",
+      "image": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSH-ZyBZr-4egsRsDzy9JjuJNQ8HAzQEyuQFw&s",
+    }
+    ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Contact List'),
-        backgroundColor: Colors.black,
+        title: Text('Dynamic List'),
+        backgroundColor: Colors.black87,
         foregroundColor: Colors.white,
         centerTitle: true,
       ),
-      drawer: drawerSide.DrawerSide(),
-      body: ListView.builder(
-        itemCount: users.length,
+      body:Center(
+        child:  ListView.builder(
+        itemCount: Cars.length,
         itemBuilder: (context, index){
-          return ListTile(
-            title: Text('${users[index]['name']}' ?? 'No Name'),
-            subtitle: Text('This is Bhai Users'),
-            leading: Icon(Icons.person, color: Colors.blue, size: 40),
-            trailing: Icon(Icons.phone, color: Colors.grey, size: 20),
+          return Padding(
+            padding: EdgeInsets.all(10),
+            child: ListTile(
+            title: Text('${Cars[index]['name']}'),
+            subtitle: Text('Car Model: ${Cars[index]['model']} \nCar Color: ${Cars[index]['color']}'),
+            leading: Image.network('${Cars[index]['image']}'),
+            trailing: Text('Car Price: ${Cars[index]['price']}', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 12),)
+          ),
           );
-        }),
+        },
+      )
+      ),
+      // body: ListView.builder(
+      //   itemCount: Users.length,
+      //   itemBuilder: (context, index){
+      //     return ListTile(
+      //       title: Text('${Users[index]['name']}'),
+      //       subtitle: Text('This is our BHAI users!!'),
+      //       leading: Icon(Icons.person),
+      //       trailing: Icon(Icons.arrow_forward_ios),
+      //     );
+      //   },
+      // ),
+      drawer: drawerSide.DrawerSide(),
     );
   }
 }
