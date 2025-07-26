e851c90bb32dd4076cff7fe35c0469bcc4ae988b
owaisk23 <owais1566@gmail.com>
2025-07-26 11:02:55 +0500

    added splash screen

--- Mob App Development/Flutter/crud/lib/main.dart
+++ Mob App Development/Flutter/crud/lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:crud/firebase_options.dart';
 import 'package:crud/products.dart';
 import 'package:crud/signup.dart';
+import 'package:crud/splashScreen.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
@@ -13,7 +14,11 @@ void main()async {
   runApp(
     MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Signup(),
+      home: SplashScreen(),
+      routes: {
+        "/signup": (context) => Signup(),
+        "/products": (context) => Products()
+      },
     )
   );
 }
---
+++ Mob App Development/Flutter/crud/lib/splashScreen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:crud/products.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({ Key? key }) : super(key: key);
+
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 3), () {
+      // Navigator.pushReplacementNamed(context, "/products");
+      // Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>Products()));
+      Navigator.pushNamed(context, "/signup");
+    });
+  }   
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.deepPurple,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.flutter_dash, size: 100, color: Colors.white,),
+            Text(
+              "My App",
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.white
+              ),)
+          ],
+        ),
+      ),
+    );
+  }
+}
