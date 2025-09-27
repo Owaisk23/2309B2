0724f6ff3349f05fe37aa47aa0f742129bc928b5
owaisk23 <owais1566@gmail.com>
2025-09-27 10:25:42 +0500

    added useState

--- MERN/React/firstproject/src/App.jsx
+++ MERN/React/firstproject/src/App.jsx
@@ -1,6 +1,8 @@
 
 import MyName from "./components/MyName";
 import Navbar from "./components/Navbar";
+import carImg from "./assets/porshe.jpg"
+import { useState } from "react";
 
 function App() {
 
@@ -15,24 +17,69 @@ function App() {
     { name: "Moin", profession: "UIUX Designer" },
   ]
 
+  // let count = 0;
+
+  // function handleIncrement() {
+  //   count++;
+  //   console.log("Incremented:", count);
+  // }
+
+  //useState Hook
+  //1.Import useState
+  //2.Declare a state variable
+  //3.Use the state variable in the component
+  //4.Update the state variable using the setter function
+
+  // const [count, setCount] = useState(0);
+
+  // const [user, setUser] = useState({ name: "Ashar", profession: "FrontEnd Developer" });
+  
+  const [std, setStd]  = useState(["Ashar", "Luqman", "Ayan Shakeel"]);
+
+
+  // const handleIncrement = () => {
+  //   // count++;
+  //   setCount(count + 1);
+  //   console.log("Incremented:", count+1);
+  // }
+
+  const handleUserUpdate = () => {
+    // setUser({ name: "Luqman", profession: "Flutter Developer" });
+
+    setStd([...std, "Huzaifa"]);
+    console.log("User Updated:", std);
+  }
+
   return (
 
     console.log("App component rendered"),
 
     <>
       <Navbar />
-      <h1>Hello, in React App!</h1>
+      <h1 className="heading">Hello, in React App!</h1>
       {/* <h2>{userData[1].name} {userData[1].profession}</h2>
       <h2>{userData[3].name} {userData[3].profession}</h2> */}
       {/* <MyName name="Ashar" profession="FrontEnd Developer"/>
       <MyName name="Luqman" profession="Flutter Developer"/> */}
 
-      {
+      {/* {
         userData.map((data, index) => {
           return <MyName key={index} name={data.name} profession={data.profession} />
         })
       }
-    
+     */}
+
+      {/* <img src={carImg} alt="" width={300} height={200} /> */}
+      {/* <h1>{count}</h1>
+      <button className="btn btn-primary" onClick={handleIncrement}>Increment</button> */}
+      {/* <h1>Name: {user.name} Profession: {user.profession}</h1> */}
+      <h1>Students List:</h1>
+      {
+        std.map((student, index) => {
+          return <h2 key={index}>{index + 1}. {student}</h2>
+        })
+      }
+      <button className="btn btn-primary" onClick={handleUserUpdate}>Update User</button>
     </>
   );
 }
--- MERN/React/firstproject/src/components/MyName/index.jsx
+++ MERN/React/firstproject/src/components/MyName/index.jsx
@@ -1,12 +1,14 @@
 import React from "react";
+// import "./index.css"; 
+import styles from "./index.module.css";
 
 const MyName = (props) => {
   return (
     <>
-      <h1>My name is {props.name}.</h1>
+      <h1 className={`${styles.heading} bg-warning `}>My name is {props.name}.</h1>
       <h2>My profession is {props.profession}.</h2>
     </>
   );
 };
 
-export default MyName;
+export default MyName; 
