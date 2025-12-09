4c5e65573a1262b556d7aad14b64a0cc1bc3fd82
owaisk23 <owais1566@gmail.com>
2025-12-09 10:13:06 +0500

    added send email

--- MERN/ExpressJS/basics/routes/productRoutes.mjs
+++ MERN/ExpressJS/basics/routes/productRoutes.mjs
@@ -1,16 +1,17 @@
 import productController from "../controller/productController.mjs";
 import express from 'express';
+import userController from "../controller/userController.mjs";
 import { upload } from "../cloudinaryConfig.mjs";
 
 const productRouter = express.Router();
 
 //fetch all products
 productRouter
-.get('/', productController.index)
+.get('/', userController.auth, productController.index)
 .get('/:id', productController.singleProduct)
 // .post('/', productController.create)
 // .post('/add', upload.single('image'), productController.addProductWithImage)
-.post('/add', upload.array('image'), productController.addProductWithImage)
+.post('/add', userController.auth, upload.array('image'), productController.addProductWithImage)
 // .post('/', productController.addProduct)
 .delete('/:id', productController.deleteProduct)
 .put('/:id', productController.editProduct);
