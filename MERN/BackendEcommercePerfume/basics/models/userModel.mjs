add7e1515cc3fe6ac77afa6d56e3de765aec545b
owaisk23 <owais1566@gmail.com>
2025-12-16 10:14:50 +0500

    added full stack

---
+++ MERN/BackendEcommercePerfume/basics/models/userModel.mjs
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  username: {type: String, required: [true, "Username is required"]}, // String is shorthand for {type: String}
+  email: {type: String, RegExp: /.+\@.+\..+/, required: [true, "Email is required"]},
+  password: {type: String, required: [true, "Password is required"]},
+  profilePicture: {type: String},
+  role: {type: String, default: "user"},
+  isVerified: {type: Boolean, default: false},
+  isActive: {type: Boolean, default: true}
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
